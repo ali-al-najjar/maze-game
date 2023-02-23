3f554d08398165635dbfd33b4bfbdf818568b3d9
aliynajjar <ali.y.alnajjar@gmail.com>
2023-02-23 18:56:40 +0200

    Added everything except the scoring system

--- game.js
+++ game.js
@@ -17,35 +17,39 @@ function resetColor(){
   }
   }
 
+
+// console.log(game_win);
+
 game_start.addEventListener("mouseover",function(){
     for(i=0;i<game_boundaries.length;i++){
       game_boundaries[i].addEventListener("mouseover",function(){
-      changeColor();
-      game_status.style.color="red";
-      game_status.innerHTML = "You lost!";
-      game_end.addEventListener("mouseover", function() {
-        game_status.innerHTML = "Return Back to the starting point";
+        game_win=false;
+        changeColor();
         game_status.style.color="red";
+        game_status.innerHTML = "You lost!";
+        game_end.addEventListener("mouseover", function() {
+          game_status.innerHTML = "Return Back to the starting point";
+          game_status.style.color="red";
       })
-      game_win=false;
+
       game_start.addEventListener("mouseover", function() {
         game_status.innerHTML = "Click on the start square to retry!";
         game_status.style.color="red";
       })
-      console.log(game_win);
+      // console.log(game_win);
     })
     }
     game_start.addEventListener("click",function(){
     resetColor();
+    game_win=true;
     game_status.innerHTML = 'Begin by moving your mouse over the "S".';
     game_status.style.color="black";
-    if(game_win == true){
     game_end.addEventListener("mouseover", function() {
         game_status.innerHTML = "YOU WIN!";
         game_status.style.color ="black";
         game_win=true;
-      })}
-      console.log(game_win);
+      })
+      // console.log(game_win);
 
   })
 
