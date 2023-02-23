58041a32114de73c6f3b1122ac8661c4606f5383
aliynajjar <ali.y.alnajjar@gmail.com>
2023-02-23 22:56:29 +0200

    Added new functions and restructured js file

--- game.js
+++ game.js
@@ -3,60 +3,70 @@ var game_boundaries = document.getElementsByClassName("boundary");
 var game_start = document.getElementById("start");
 var game_end = document.getElementById("end");
 var game_status = document.getElementById("status");
-var game_win= true;
+var score = 0;
+var example = document.getElementsByClassName("example")[0];
 
 function changeColor(){
-  for(i=0;i<game_boundaries.length;i++){
+  for(i=0;i<game_boundaries.length-1;i++){
     game_boundaries[i].style.borderColor="red";
   }
   }
 
 function resetColor(){
-  for(i=0;i<game_boundaries.length;i++){
+  for(i=0;i<game_boundaries.length-1;i++){
     game_boundaries[i].style.borderColor="black";
   }
   }
 
-
-// console.log(game_win);
-
-game_start.addEventListener("mouseover",function(){
-    for(i=0;i<game_boundaries.length;i++){
-      game_boundaries[i].addEventListener("mouseover",function(){
-        game_win=false;
-        changeColor();
-        game_status.style.color="red";
-        game_status.innerHTML = "You lost!";
-        game_end.addEventListener("mouseover", function() {
-          game_status.innerHTML = "Return Back to the starting point";
-          game_status.style.color="red";
-      })
-
-      game_start.addEventListener("mouseover", function() {
-        game_status.innerHTML = "Click on the start square to retry!";
-        game_status.style.color="red";
-      })
-      // console.log(game_win);
-    })
+function gameLost(){
+  changeColor();
+  score -= 10;
+  example.innerText= score;
+  game_status.style.color="red";
+  game_status.innerHTML = "You lost!";
+  for(i=0;i<game_boundaries.length;i++){
+      game_boundaries[i].removeEventListener("mouseover",gameLost)
+    
     }
-    game_start.addEventListener("click",function(){
-    resetColor();
-    game_win=true;
-    game_status.innerHTML = 'Begin by moving your mouse over the "S".';
-    game_status.style.color="black";
-    game_end.addEventListener("mouseover", function() {
-        game_status.innerHTML = "YOU WIN!";
-        game_status.style.color ="black";
-        game_win=true;
-      })
-      // console.log(game_win);
+  game_end.removeEventListener("mouseover",gameEnd)
+  }
 
-  })
+function gameStart(){
+  resetColor();
+  game_status.innerHTML = 'Begin by moving your mouse over the "S".';
+  game_status.style.color="black";
+  for(i=0;i<game_boundaries.length;i++){
+    game_boundaries[i].addEventListener("mouseover",gameLost)
+  
+  }
+  game_end.addEventListener("mouseover",gameEnd)
+}
 
-})
+function gameEnd(){
+  game_status.innerHTML = "YOU WIN!";
+  game_status.style.color ="black";
+  score += 5;
+  example.innerText= score;
+  game_end.removeEventListener("mouseover",gameEnd)
+  for(i=0;i<game_boundaries.length;i++){
+    game_boundaries[i].removeEventListener("mouseover",gameLost)
+}}
 
+function gameReset(){
+  resetColor();
+  game_win=true;
+  score = 0;
+  example.innerText= score;
+  game_status.innerHTML = 'You resetted the game! Begin by moving your mouse over the "S".';
+  game_status.style.color="black";
+  for(i=0;i<game_boundaries.length;i++){
+    game_boundaries[i].addEventListener("mouseover",gameLost)
+  
+  }
+  game_end.addEventListener("mouseover",gameEnd)
+}
 
-
-
+game_start.addEventListener("mouseover",gameStart);
+game_start.addEventListener("click",gameReset);
 
 })
